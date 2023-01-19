271d5e216459a96bc60280e2fc4066f286a16e3b
csingh1108 <113258536+csingh1108@users.noreply.github.com>
2023-01-19 10:44:01 -0800

    Minor tweak

--- Assets/Scripts/HighscoreTable.cs
+++ Assets/Scripts/HighscoreTable.cs
@@ -11,7 +11,7 @@ public class HighscoreTable : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.H))
+        if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             reverse = !reverse;
             highscoretable.SetActive(reverse);
